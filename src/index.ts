a801510d407d61ccf8f60afeab88f7d7ef24225b
yourgotocoder <yourgotocoder@gmail.com>
2022-05-09 14:55:45 +0530

    Importing using aliases

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import multiply, { multiplyBy2 } from "./multiply";
+import multiply, { multiplyBy2 as mBy2 } from "./multiply";
 const a = 4;
 const b = 5;
 
 console.log(multiply(a, b));
+console.log(mBy2(a, b));
