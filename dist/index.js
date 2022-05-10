b932728040c61f3419b4e183363780db0609a8ad
yourgotocoder <yourgotocoder@gmail.com>
2022-05-10 14:32:10 +0530

    Function overloading

--- dist/index.js
+++ dist/index.js
@@ -3,3 +3,14 @@ let a = { someProp: 4 };
 let b = a;
 const add = (a, b) => a + b;
 const child = { surname: "R" };
+const sum2 = (a, b) => a + (b || 0); //Providing a default value
+function sumEverything(...numbers) {
+    return numbers.reduce((prevValue, currentValue) => prevValue + currentValue);
+}
+console.log(sumEverything(2, 3, 5));
+function calcArea(...args) {
+    if (args.length === 2) {
+        return args[0] * args[1];
+    }
+    return args[0] * args[0];
+}
--- src/index.ts
+++ src/index.ts
@@ -28,3 +28,26 @@ interface Parent {
 interface Child extends Parent {}
 
 const child: Child = { surname: "R" };
+//Functions
+type myFunc = (a: number, b?: number) => number;
+const sum2: myFunc = (a, b) => a + (b || 0); //Providing a default value
+
+function sumEverything(...numbers: number[]): number {
+    return numbers.reduce(
+        (prevValue, currentValue) => prevValue + currentValue
+    );
+}
+
+console.log(sumEverything(2, 3, 5));
+
+//Overloads
+
+function calcArea(width: number, height: number): number;
+function calcArea(length: number): number;
+
+function calcArea(...args: number[]): number {
+    if (args.length === 2) {
+        return args[0] * args[1];
+    }
+    return args[0] * args[0];
+}
