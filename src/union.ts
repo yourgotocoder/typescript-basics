c03a564b78143ff4dc7ab1ad0a5a185f63930598
yourgotocoder <yourgotocoder@gmail.com>
2022-05-12 16:04:29 +0530

    interface guards????

--- src/union.ts
+++ src/union.ts
@@ -7,3 +7,25 @@ function someFn(myArgument: number | string | boolean) {
     }
     return myArgument;
 }
+
+interface Cat {
+    meow(): void;
+    walk(): void;
+}
+interface Dog {
+    bark(): void;
+    walk(): void;
+}
+
+function isDog(someObj: Dog | Cat): someObj is Dog {
+    return (<Dog>someObj).bark !== undefined;
+}
+
+function callMyPet(pet: Dog | Cat) {
+    pet.walk();
+    if (isDog(pet)) {
+        pet.bark();
+    } else {
+        pet.meow();
+    }
+}
