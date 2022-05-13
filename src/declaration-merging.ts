32a20c8507854d38e0ef71094ce5c19f0e16e73b
yourgotocoder <yourgotocoder@gmail.com>
2022-05-13 09:49:03 +0530

    More on declaration merging

--- src/declaration-merging.ts
+++ src/declaration-merging.ts
@@ -19,3 +19,31 @@ let myCart: Cart = {
         return 1;
     },
 };
+
+//Namespace
+
+namespace MyNamespace {
+    export const x: number = 10;
+    export interface SomeInterface {
+        y: number;
+    }
+}
+
+namespace MyNamespace {
+    export const getX = () => x;
+    export interface SomeInterface {
+        x: number;
+    }
+}
+
+MyNamespace.getX();
+
+const someInterface: MyNamespace.SomeInterface = {
+    x: 1,
+    y: 2,
+};
+
+enum Vegetables {
+    Tomato = "tomato",
+    Onion = "onion",
+}
